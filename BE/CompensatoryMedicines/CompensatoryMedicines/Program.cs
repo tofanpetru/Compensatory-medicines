1f5aaf6d9261cf9300e47e820ed483923a61b832
Tofan Petru <tofanpetru12@gmail.com>
2023-03-29 12:54:51 +0300

    identity small updates

--- BE/CompensatoryMedicines/CompensatoryMedicines/Program.cs
+++ BE/CompensatoryMedicines/CompensatoryMedicines/Program.cs
@@ -15,7 +15,7 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
